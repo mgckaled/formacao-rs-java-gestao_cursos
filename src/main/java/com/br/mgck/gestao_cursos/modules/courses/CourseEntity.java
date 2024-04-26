a51375d6ad043d4a254d86bff3210b8ce685f668
mgckaled <marcelgckaled@gmail.com>
2024-04-26 17:04:34 -0300

    feat: add course entity with some validations

---
+++ src/main/java/com/br/mgck/gestao_cursos/modules/courses/CourseEntity.java
@@ -0,0 +1,40 @@
+package com.br.mgck.gestao_cursos.modules.courses;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+@Entity(name = "course")
+public class CourseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    private String name;
+
+    private String category;
+
+    @NotBlank
+    private String status;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
--- src/main/java/com/br/mgck/gestao_cursos/modules/courses/controllers/CourseController.java
+++ src/main/java/com/br/mgck/gestao_cursos/modules/courses/controllers/CourseController.java
@@ -1,13 +1,19 @@
 package com.br.mgck.gestao_cursos.modules.courses.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.br.mgck.gestao_cursos.modules.courses.CourseEntity;
 
 @RestController
 @RequestMapping("/course")
 public class CourseController {
-    
-    public void create() {
 
+    @PostMapping("/")
+    public void create(@RequestBody CourseEntity courseEntity) {
+        System.out.println("Curso");
+        System.out.println(courseEntity.getName());
     }
 }
