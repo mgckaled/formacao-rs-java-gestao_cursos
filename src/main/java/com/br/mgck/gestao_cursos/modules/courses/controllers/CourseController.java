01b76f175a9285347594a59981c1c3121dd702b1
mgckaled <marcelgckaled@gmail.com>
2024-04-26 16:07:40 -0300

    create spring boot project with maven

---
+++ src/main/java/com/br/mgck/gestao_cursos/modules/courses/controllers/CourseController.java
@@ -0,0 +1,13 @@
+package com.br.mgck.gestao_cursos.modules.courses.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/course")
+public class CourseController {
+    
+    public void create() {
+
+    }
+}
